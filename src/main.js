41c500881970933516095c1ef59429c37a98d339
HZX-2020 <1016261360@qq.com>
2020-11-14 17:52:01 +0800

    系统初始布局

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue';
+import './plugins/ant-design-vue';
 import App from './App.vue';
+
 import router from './router';
 import store from './store';
+import '@/assets/css/reset.less';
 
 Vue.config.productionTip = false;
 
